85bcd2b58004e32b164b58549b0a3428d7c36873
UnknownCreator1 <90321114+UnknownCreator1@users.noreply.github.com>
2022-04-19 16:12:50 +0100

    initial

--- src/index.js
+++ src/index.js
@@ -4,3 +4,53 @@ console.log(data);
 //You can start simple and just render a single 
 //pokemon card from the first element
 console.log(data[0]);
+
+
+// 1
+// const cardElement = document.createElement('li')
+// cardElement.setAttribute('class', 'card')
+// document.body.append(cardElement)
+
+// 2
+// const ul = document.getElementById('cards');
+// const ul = document.getElementsByClassName('cards');
+// document.body.append(ul)
+// const listItems = ul.getElementsByTagName('li');
+// document.body.append(li)    
+// // Loop through the NodeList object.
+// for (let i = 0; i <= listItems.length - 1; i++) {
+//     console.log (listItems[i]);
+// }
+// lisItems.setAttribute('class','card')
+
+// 3
+
+const cardListUL = document.getElementById('card-list')
+for (let i = 0; i <= data.length; i++) {
+    console.log("Pokemon at i", data[i])
+    const pokemon = data[i] // js object of current pokemon
+
+    const cardLI =  document.creatElement('li')
+  // apply a style or a css class
+    cardLI.setAttribute('class','card')
+    cardListUL.append(cardLI) // at the end of the list
+  // create more elements and append to cardLI
+  // create an h2, apply title style, add name of pokemon to innerHtml
+  // create an h2
+    const listH2 = document.createElement('h2')
+  // set the text to pokemon name
+    listH2.innerHTML = pokemon.name
+  // add h2 to li
+    cardLI.append(listH2)
+
+    // listH2.innerText = 'Bulbasaur'
+    // listH2.setAttribute('class','card--title')
+    // cardLI.append(listH2)
+    // const listImg = document.createElement('img')
+    // cardLI.append(listImg)
+    // listImg.setAttribute('class', 'width = "256" ')
+    
+}
+
+
+
